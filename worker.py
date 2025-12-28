60d9488c437d574a1a8e3ecfda24f41781677a54
scappugi <simone.cappugi@stud.unifi.it>
2025-12-28 11:38:48 +0100

    corretto metodo

--- worker.py
+++ worker.py
@@ -11,6 +11,7 @@ import time
 toShare = []
 _email_manager = EmailManager.EmailManager()
 
+
 # Metodo per Mapper
 def process_chunk(args):
     raw_emails_chunk, m, k = args
@@ -20,23 +21,22 @@ def process_chunk(args):
     for raw_email in raw_emails_chunk:
         # Normalizza l'email
         email = _email_manager.normalize_email(raw_email)
-
         indices = BloomFilter.BloomFilter.calculate_hashes(email, m, k)
         all_indices.extend(indices)
 
     return all_indices
+
 
 def process_chunk_shared(args):
     raw_emails_chunk, m, k = args
     # Per ogni email nel pacchetto
     for raw_email in raw_emails_chunk:
         email = _email_manager.normalize_email(raw_email)
-
-        for i in range(k):
-            index = mmh3.hash(str(email), i) % m
-
-            # Aggiorna l'array di bit condiviso
+        indices = BloomFilter.BloomFilter.calculate_hashes(email, m, k)
+        # Aggiorna l'array di bit condiviso
+        for index in indices:
             toShare[index] = 1
+
 
 def init_worker_shared(shared_array):
     global toShare
